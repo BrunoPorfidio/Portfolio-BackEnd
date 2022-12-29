9335e78321c3da2327819a547498015e6636e38b
BrunoPorfidio <thewarriours7@gmail.com>
2022-12-29 16:45:26 -0300

    Actializando datos del Esquma

--- src/main/java/com/miportfolio/primerportfolio/model/Usuario.java
+++ src/main/java/com/miportfolio/primerportfolio/model/Usuario.java
@@ -1,13 +1,27 @@
 
 package com.miportfolio.primerportfolio.model;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
 public class Usuario {
     
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Basic
     private Long id;
     private String nombre;
     private String apellido;
     private String correo;
-    private String contrasenia;
+    private String password;
 
     public Usuario() {
     }
@@ -17,48 +31,6 @@ public class Usuario {
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
-        this.contrasenia = contrasenia;
+        this.password = contrasenia;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getContrasenia() {
-        return contrasenia;
-    }
-
-    public void setContrasenia(String contrasenia) {
-        this.contrasenia = contrasenia;
-    }
-
-    
 }
