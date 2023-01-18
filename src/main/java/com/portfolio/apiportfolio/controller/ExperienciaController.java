b216686a97d232939610752e7ea76084b2f02e91
BrunoPorfidio <thewarriours7@gmail.com>
2023-01-18 15:43:13 -0300

    Quitando importaciones

--- src/main/java/com/portfolio/apiportfolio/controller/ExperienciaController.java
+++ src/main/java/com/portfolio/apiportfolio/controller/ExperienciaController.java
@@ -1,14 +1,11 @@
 package com.portfolio.apiportfolio.controller;
 
 import com.portfolio.apiportfolio.model.Experiencia;
-import com.portfolio.apiportfolio.model.Skills;
 import com.portfolio.apiportfolio.repository.ExperienciaRepo;
 import com.portfolio.apiportfolio.service.IExperienciaService;
 import java.util.List;
-import java.util.Optional;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
--- src/main/java/com/portfolio/apiportfolio/controller/ProyectoController.java
+++ src/main/java/com/portfolio/apiportfolio/controller/ProyectoController.java
@@ -1,14 +1,11 @@
 package com.portfolio.apiportfolio.controller;
 
-import com.portfolio.apiportfolio.model.Persona;
 import com.portfolio.apiportfolio.model.Proyectos;
 import com.portfolio.apiportfolio.repository.ProyectoRepo;
 import com.portfolio.apiportfolio.service.IProyectoService;
 import java.util.List;
-import java.util.Optional;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
