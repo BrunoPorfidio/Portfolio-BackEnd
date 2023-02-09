027a2e8a8d7868f12decb3a2c9d3f281b3eee421
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-02-08 21:28:09 -0300

    Corrigiendo CORS

--- src/main/java/com/portfolio/apiportfolio/WebConfig.java
+++ src/main/java/com/portfolio/apiportfolio/WebConfig.java
@@ -1,18 +1,29 @@
-package com.portfolio.apiportfolio;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@EnableWebMvc
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
- @Override
- public void addCorsMappings(CorsRegistry registry) {
- registry.addMapping("/**");
- }
-
-}
-    
+//package com.portfolio.apiportfolio;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class WebConfig implements Filter{
+//
+// @Override
+// public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//        chain.doFilter(req, res);
+//    }
+//
+// @Override
+//    public void init(FilterConfig filterConfig) {}
+//
+// @Override
+//    public void destroy() {}
+//
+//
+//}
+//    
