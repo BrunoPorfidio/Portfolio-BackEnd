f28438ed5a17f8ccc2fc5f83cbaf34ffa86ee780
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-02-24 21:11:36 -0300

    Volviendo a Java 17

--- src/main/java/com/portfolio/api/ApiApplication.java
+++ src/main/java/com/portfolio/api/ApiApplication.java
@@ -1,12 +1,8 @@
 package com.portfolio.api;
 
-import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @SpringBootApplication
 public class ApiApplication {
@@ -14,19 +10,4 @@ public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
-//        configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
-//        configuration.setMaxAge(3600L);
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-
 }
