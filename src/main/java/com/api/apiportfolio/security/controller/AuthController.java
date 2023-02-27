fe9f685db7aaddf0d1dee8318c254bb548147911
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-02-27 17:52:03 -0300

    Mejorando Seguridad

--- src/main/java/com/api/apiportfolio/security/controller/AuthController.java
+++ src/main/java/com/api/apiportfolio/security/controller/AuthController.java
@@ -19,7 +19,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -84,9 +83,7 @@ public class AuthController {
 
 		String jwt = jwtProvider.GenerateToken(authentication);
 
-		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
-		JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+		JwtDto jwtDto = new JwtDto(jwt);
 		return new ResponseEntity(jwtDto, HttpStatus.OK);
 	}
 }
--- src/main/java/com/api/apiportfolio/security/jwt/JwtProvider.java
+++ src/main/java/com/api/apiportfolio/security/jwt/JwtProvider.java
@@ -8,45 +8,58 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtProvider {
-	private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
-	
-	@Value("${jwt.secret}")
-	private String secret;
-	@Value("${jwt.expiration}")
-	private int expiration;
-	
-	public String GenerateToken (Authentication authentication){
-		UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
-		return Jwts.builder().setSubject(usuarioPrincipal.getUsername()).setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime()+expiration*1000)).signWith(SignatureAlgorithm.HS512,secret).compact();
-	}
-	
-	public String getNombreUsuarioFromToken (String token) {
-		return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
-	}
-	
-	public boolean validateToken (String token) {
-		try{
-			Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
-			return true;
-	} catch (MalformedJwtException e) {
-		logger.error("Token mal formado");
-	}catch (UnsupportedJwtException e) {
-		logger.error("Token no soportado");
-	}catch (ExpiredJwtException e) {
-		logger.error("Token expirado");
-	}catch (IllegalArgumentException e) {
-		logger.error("Token vacio");
-	}catch (SignatureException e) {
-		logger.error("Firma no válida");
-	}
-		return false;
-	}
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    public String GenerateToken(Authentication authentication) {
+        UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
+        List<String> roles = usuarioPrincipal.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        return Jwts.builder()
+                .setSubject(usuarioPrincipal.getUsername())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date()
+                        .getTime() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
+    }
+
+    public String getNombreUsuarioFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Token mal formado");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Token no soportado");
+        } catch (ExpiredJwtException e) {
+            logger.error("Token expirado");
+        } catch (IllegalArgumentException e) {
+            logger.error("Token vacio");
+        } catch (SignatureException e) {
+            logger.error("Firma no válida");
+        }
+        return false;
+    }
 }
