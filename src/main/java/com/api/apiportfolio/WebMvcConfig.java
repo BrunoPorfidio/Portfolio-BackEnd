2fe4a15bf3630ac6453eda1c177b0d8e24b4a63e
Bruno Porfidio <83902191+BrunoPorfidio@users.noreply.github.com>
2023-03-11 18:39:25 -0300

    mejorando config de CORS

--- src/main/java/com/api/apiportfolio/WebMvcConfig.java
+++ src/main/java/com/api/apiportfolio/WebMvcConfig.java
@@ -7,14 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
+public class WebMvcConfig implements WebMvcConfigurer {
+ 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedMethods("*")
             .allowedHeaders("*")
-            .allowCredentials(true);
+            .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
+ 
 }
